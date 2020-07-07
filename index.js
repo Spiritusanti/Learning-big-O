b8b3f555f5b962f331783b33c5d73c2c4e5a9051
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-07 16:51:30 +0000

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// let's get scallable!
+// let's get scalable!
+// this is our data
 const nemo = ['nemo'];
 
+// we told the machine we wanted to find nemo given an array. We did this by looping through the array until the if statement evauluates to true.
 function findNemo(array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
