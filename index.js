1a4ed2c2790fa6790a78daa7103514795d5604ae
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-07 16:48:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let's get scallable!
+const nemo = ['nemo'];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('found Nemo!');
+    }
+  }
+}
+
+findNemo(nemo);
